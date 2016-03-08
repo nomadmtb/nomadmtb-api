59e50f114e8b171ddfd424659831646c07f24ad1
Kyle Luce <kylegluce@gmail.com>
2016-03-07 19:30:41 -0600

    Model changes + .DS_Store

--- nomadmtb/blog/admin.py
+++ nomadmtb/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Post, Link, Profile
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Link)
+admin.site.register(Profile)
--- nomadmtb/blog/models.py
+++ nomadmtb/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+	"""This model will represent a Post object in the application"""
+
+	def __str__(self):
+		return "post(pk={0}) >>> \"{1}\"".format(self.id, self.title)
+
+	author = models.ForeignKey(User)
+	title = models.CharField(max_length=200, blank=False)
+	date = models.DateTimeField(auto_now_add=True, blank=False)
+	text = models.TextField(blank=False)
+
+class Link(models.Model):
+	"""This model will represent a Link object in the application"""
+
+	def __str__(self):
+		return "link(pk={0}) >>> \"{1}\"".format(self.id, self.title)
+
+	post = models.ForeignKey(Post)
+	url = models.URLField(max_length=350, blank=False)
+	title = models.CharField(max_length=200, blank=False)
+
+class Profile(models.Model):
+	"""This model will represent a Profile for each User in the applicaiton."""
+
+	def __str__(self):
+		return "profile(pk={0})".format(self.id)
+
+	bio = models.TextField(blank=False)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
